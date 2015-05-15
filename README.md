0cbbbcd3ba2b938123d5eaca95d369fc076c2c93
Arnaud Benard <arnaudbenard13@gmail.com>
2015-05-15 16:57:29 +0100

    Inital commit with basic commands

---
+++ README.md
@@ -0,0 +1,14 @@
+# hlcode
+
+## Installation
+
+You have install highlight on your machine
+
+```
+brew install highlight
+```
+
+
+## Supported languages
+
+[List](http://www.andre-simon.de/doku/highlight/en/langs.php)
---
+++ index.js
@@ -0,0 +1,23 @@
+var exec = require('child_process').exec;
+var minimist = require('minimist');
+var argv = minimist(process.argv.slice(2), {
+    alias: {
+        s: 'syntax',
+        f: 'file'
+    }
+});
+
+var cmd = '';
+var syntax = argv.s;
+var filename = argv.f;
+
+if (filename) {
+    return exec('highlight -O rtf ' + filename + ' | pbcopy');
+}
+
+if (!syntax) {
+    return console.warn('No syntax specified. Use the -s flag.');
+}
+
+return exec('pbpaste | highlight --syntax=' + syntax + ' -O rtf | pbcopy')
+
